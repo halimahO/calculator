d21b0929624bf6468bfc72423f768dc022002adc
Halimah Oladosu <halimah.oladosu@revelsystems.com>
2021-07-19 11:22:52 +0300

    Add init validation to tests

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -8,30 +8,10 @@ class Calculator:
     ----------
     memory_value : float
         value in calculator memory
+    """
 
-    Methods
-    -------
-    add(self, input_value):
-        Adds input value to memory value.
-
-    subtract(self, input_value):
-        Subtracts input value from memory value.
-
-    divide(self, input_value):
-        Divides input value by memory value.
-
-    multiply(self, input_value):
-        Multiplies input value by memory value.
-
-    root(self, input_value):
-        Returns the input value root of the memory value.
-
-    reset(self):
-        Resets memory value to zero.
-        """
-
-    def __init__(self, memory_value: float = 0) -> float:
-        self._memory_value = memory_value
+    def __init__(self, memory_value: float = 0):
+        self._memory_value = self._validate_memory_value(memory_value)
         """
         Constructs the necessary attributes for the calculator object.
 
@@ -43,48 +23,61 @@ class Calculator:
 
     def add(self, input_value: float) -> float:
         """Takes in a value and add it to the value in memory, returns the addition."""
-        self.validate_input(input_value)
+        self._validate_input(input_value)
         self._memory_value += input_value
         return self._memory_value
 
     def subtract(self, input_value: float) -> float:
         """Takes in a value and subtract it from the value in memory, returns the subtraction."""
-        self.validate_input(input_value)
+        self._validate_input(input_value)
         self._memory_value -= input_value
         return self._memory_value
 
     def multiply(self, input_value: float) -> float:
         """Takes in a value and multiply it with the value in memory, returns the multiplication."""
-        self.validate_input(input_value)
+        self._validate_input(input_value)
         self._memory_value *= input_value
         return self._memory_value
 
     def divide(self, input_value: float) -> float:
         """Takes in a value and divide it by the value in memory, returns the division."""
-        self.validate_input(input_value)
-        self.division_by_zero(input_value)
+        self._validate_input(input_value)
+        self._division_by_zero(input_value)
         self._memory_value /= input_value
         return self._memory_value
 
     def root(self, input_value: float) -> float:
         """Takes in a value n, return the nth root of the value in memory."""
-        self.validate_input(input_value)
-        self.division_by_zero(input_value)
+        self._validate_input(input_value)
+        self._division_by_zero(input_value)
+        self._complex_number_for_root(input_value)
         self._memory_value = self._memory_value ** (1.0 / input_value)
         return self._memory_value
 
 
-    def reset(self):
+    def reset(self) -> float:
         """Resets the value in memory to zero."""
         self._memory_value = 0
         return self._memory_value
 
-    def validate_input(self, input_value):
+    def _validate_memory_value(self, new_value: float) -> float:
+        if not isinstance(new_value, (int, float)):
+            raise ValueError("Please initialize calculator with a valid number for memory value!")
+        self._memory_value = new_value
+        return self._memory_value
+
+    def _validate_input(self, input_value: float) -> float:
         if isinstance(input_value, (int, float)):
             return input_value
         raise TypeError("Please input a valid number!")
 
-    def division_by_zero(self, input_value):
+    def _division_by_zero(self, input_value: float) -> float:
         if input_value == 0:
             raise ZeroDivisionError("Division by zero is invalid!")
+        return input_value
+
+
+    def _complex_number_for_root(self, input_value: float) -> float:
+        if self._memory_value < 0:
+            raise ValueError("Root of complex or negative numbers is not allowed")
         return input_value
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,43 +1,50 @@
 import pytest
 from src.calculator import calculator
 
-calculator = calculator.Calculator()
 
 def test_add():
-    calculator.reset()
-    assert calculator.add(4) == 4, "Addition method fails"
+    calc = calculator.Calculator()
+    assert calc.add(4) == 4, "Addition method fails"
 
 def test_subtract():
-    calculator.reset()
-    assert calculator.add(4)
-    assert calculator.subtract(2) == 2, "Subtraction method fails"
+    calc = calculator.Calculator(4)
+    assert calc.subtract(2) == 2, "Subtraction method fails"
 
 def test_multiply():
-    calculator.reset()
-    assert calculator.add(4)
-    assert calculator.multiply(4) == 16, "Multiply method fails"
+    calc = calculator.Calculator(4)
+    assert calc.multiply(4) == 16, "Multiply method fails"
 
 def test_division():
-    calculator.reset()
-    assert calculator.add(4)
-    assert calculator.divide(2) == 2, "Division method fails"
+    calc = calculator.Calculator(4)
+    assert calc.divide(2) == 2, "Division method fails"
 
 def test_root():
-    calculator.reset()
-    assert calculator.add(8)
-    assert calculator.root(3) == 2, "Root method fails"
+    calc = calculator.Calculator(8)
+    assert calc.root(3) == 2, "Root method fails"
 
 def test_reset():
-    calculator.reset()
-    assert calculator.add(4)
-    assert calculator.reset() == 0, "Reset method fails"
+    calc = calculator.Calculator(5)
+    assert calc.reset() == 0, "Reset method fails"
 
 def test_type_error():
+    calc = calculator.Calculator(5)
     with pytest.raises(TypeError) as error:
-        calculator.validate_input("a")
+        calc.add("asdf")
     assert str(error.value.args[0]) == "Please input a valid number!"
 
 def test_division_by_zero():
+    calc = calculator.Calculator(5)
     with pytest.raises(ZeroDivisionError) as error:
-        calculator.division_by_zero(0)
+        calc.divide(0)
     assert str(error.value.args[0]) == "Division by zero is invalid!"
+
+def test_validate_init_method():
+    with pytest.raises(ValueError) as error:
+        calculator.Calculator("asdf")
+    assert str(error.value.args[0]) == "Please initialize calculator with a valid number for memory value!"
+
+def test_complex_number():
+    calc = calculator.Calculator(-9)
+    with pytest.raises(ValueError) as error:
+        calc.root(2)
+    assert str(error.value.args[0]) == "Root of complex or negative numbers is not allowed"
